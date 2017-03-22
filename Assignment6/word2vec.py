1a8e222ba4e54a0f4cef17a0a87a9a2aaaf4afd3
tinayliu163 <tinayliu163@gmail.com>
2017-03-22 17:38:35 -0400

    word2vec

--- Assignment6/word2vec.py
+++ Assignment6/word2vec.py
@@ -6,6 +6,7 @@ from gensim.models.word2vec import LineSentence
 import logging
 from gensim.models.word2vec import Text8Corpus
 
+
 logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s', level=logging.INFO)
 data = Text8Corpus("/Users/admin/Downloads/text8")
 model = Word2Vec (data,	size=50, window=8, min_count=5, workers=8)
