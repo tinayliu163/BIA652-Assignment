a93b2cd99d8c3f37868bb1a53ddfe0ada6b05ff4
tinayliu163 <tinayliu163@gmail.com>
2017-03-22 13:45:35 -0400

    apply PCA to reduce the data to 2D

---
+++ Assignment5/PCA.py
@@ -0,0 +1,81 @@
+
+# apply PCA to reduce the data to 2D
+
+import numpy as np
+import pandas as pd
+import matplotlib. pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import random
+
+digits_label=pd.read_csv("/Users/admin/Desktop/digits_label_random_sample.csv",index_col = 0)
+digits = pd.read_csv("/Users/admin/Desktop/digits_random_sample.csv", index_col = 0)
+
+digits2 = StandardScaler(with_std=False).fit_transform(digits)
+pca = PCA(n_components=2)
+pca.fit(digits2)
+digits_pca = pca.fit_transform(digits2)
+pca.explained_variance_ratio_.sum() # get how much variance the 2D PCA can explain
+
+#Projection to 2D
+
+digits_transposed = np.transpose(digits)
+labels_transpose = digits.columns.delete(-1)
+digits_transposed_normalized = StandardScaler(with_std=False).fit_transform(digits_transposed)
+
+pca.fit(digits_transposed_normalized)
+digits_transpose_pca = pca.fit_transform(digits_transposed_normalized)
+
+X = digits_transposed_normalized
+print ("dimension of data = ", X.shape)
+XTX = np.dot (X.T, X)
+(sigma, U) = np.linalg.eig(XTX)
+
+# The eigenvalues
+
+idx = sigma.argsort()[::-1]
+sigma = sigma[idx] # we sort so that largest eigens come first
+U = U[: idx]
+
+print(sum (sigma [0:2]))
+print(sum(sigma))
+print ("first two eigenv account for ", sum(sigma[0:2])/sum(sigma))
+
+# Projection matrix using the first two eigenvectors
+
+P = U [: :2]
+print("eigevectors = ", U , "P = " , P)
+
+# After projection
+
+pca_manual = np.dot (X, P)
+
+print ("Project data to 2D:", pca_manual)
+
+# Visualize the 2D data as a scatter plot, and annotate the dots using the digit label.
+
+# generate 77 random colors, one for each cereal
+
+random.seed(123)
+color = ["#%06x" % random.randint(0, 0xAAAAAA) for i in range(0, digits.shape[0])]
+from pylab import rcParams #set figure size
+rcParams['figure.figsize'] = 15, 15
+
+#scatter plot
+
+for x, y, c in zip(digits_pca[:,0], digits_pca[:,1], color):
+	plt.scatter(x,y,color=c)
+
+#labels
+for label, x, y, c in zip (labels, digits_pca[:,0],digits_pca[:,1], color):
+	plt.annotate(label, xy = (x, y), xytext = (-0, 0)
+	textcoords = 'offset points', ha = 'right', va = 'bottom', color=c)
+	plt.show()
+
+
+
+
+
